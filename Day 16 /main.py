91f9873cb0ba41f015376238948035dc689af743
Wojtke7 <113611677+Wojtke7@users.noreply.github.com>
2023-04-06 11:42:06 +0200

    Delete Day 16  directory

--- Day 16 /main.py
+++
@@ -1,7 +0,0 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("First column", ["One", "Two", "Three"])
-table.add_column("Second column", ["Four", "Five", "Six"])
-table.align = "l"
-print(table)
