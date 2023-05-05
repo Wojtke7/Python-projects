640e7c77db0c8ab4207470469232f3101ad0f88f
wmarcela <wmarcela@student.agh.edu.pl>
2023-05-05 16:47:52 +0200

    Saving to csv unknown words, repeating exercises, changed button commands

--- Day 31 Flash Card App/main.py
+++ Day 31 Flash Card App/main.py
@@ -1,17 +1,62 @@
 from tkinter import *
+from tkinter import messagebox
 import random
 import pandas
+import csv
 
 GREEN = "#b1ddc6"
+fields = ["English", "Polish"]
 # ------------------------- READING DATA ------------------------ #
-df = pandas.read_csv("english_polishWords.csv")
-words_dict = df.to_dict(orient="records")
-print(words_dict)
+try:
+    df = pandas.read_csv("words_to_learn.csv")
+except FileNotFoundError:
+    df = pandas.read_csv("english_polishWords.csv")
+finally:
+    words_dict = df.to_dict(orient="records")
+
+
+def correct_click():
+    dict_pos_to_delete = random_word()
+    words_dict.remove(dict_pos_to_delete)
+
+
+def wrong_click():
+    dict_pos_to_save = random_word()
+    try:
+        with open("words_to_learn.csv", mode="a", newline="") as data_file:
+            writer = csv.writer(data_file)
+
+            if data_file.tell() == 0:
+                writer.writerow(fields)
+
+            writer.writerow(dict_pos_to_save.values())
+    except FileNotFoundError:
+        with open("words_to_learn.csv", mode="w", newline="") as data_file:
+            writer = csv.writer(data_file)
+
+            writer.writerow(fields)
+
+            writer.writerow(dict_pos_to_save.values())
+
+
+def reset_app():
+    global words_dict
+    global df
+    df = pandas.read_csv("english_polishWords.csv")
+    words_dict = df.to_dict(orient="records")
 
 
 def random_word():
+    try:
+        random_position = random.choice(words_dict)
+    except IndexError:
+        msg_box = messagebox.askyesno(title="The end", message="You know all words! Would you like to try again ?")
+        if msg_box:
+            reset_app()
+            random_position = random.choice(words_dict)
+        else:
+            window.quit()
 
-    random_position = random.choice(words_dict)
     english_word = random_position["English"]
 
     canvas.itemconfig(language, text=f"English", fill="black")
@@ -19,10 +64,10 @@ def random_word():
     canvas.itemconfig(displaying_img, image=card_front)
 
     window.after(3000, flip_card, random_position)
+    return random_position
 
 
 def flip_card(random_position):
-
     polish_word = random_position["Polish"]
 
     canvas.itemconfig(displaying_img, image=card_back)
@@ -48,12 +93,12 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 # Correct button
 corr_button_img = PhotoImage(file="images/right.png")
-correct_button = Button(image=corr_button_img, highlightthickness=0, command=random_word)
+correct_button = Button(image=corr_button_img, highlightthickness=0, command=correct_click)
 correct_button.grid(row=1, column=1)
 
 # Wrong button
 wr_button_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wr_button_img, highlightthickness=0, command=random_word)
+wrong_button = Button(image=wr_button_img, highlightthickness=0, command=wrong_click)
 wrong_button.grid(row=1, column=0)
 
 random_word()
