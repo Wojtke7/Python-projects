7f447a592fefa5c5f9b9b52aed74def5082ff3b2
Wojtke7 <wojtekmarcela@interia.pl>
2024-02-20 20:20:31 +0100

    feat: feat, saving jobs problem solved, missing time.sleep() line

--- Day 49 Linkedin job offers follower bot/main.py
+++ Day 49 Linkedin job offers follower bot/main.py
@@ -5,7 +5,7 @@ import time
 passwd = "lol123passwd"
 mail = "wojciechmarcela7@gmail.com"
 
-url = "https://www.linkedin.com/jobs/search/?currentJobId=3811750220&f_AL=true&f_E=2&keywords=python%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER"
+url = "https://www.linkedin.com/jobs/search/?currentJobId=3829879442&f_AL=true&f_E=2&keywords=python%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER"
 # url2 = "https://www.linkedin.com/jobs/search/?currentJobId=3813713952&f_AL=true&f_E=1&keywords=python%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER"
 chrome_settings = webdriver.ChromeOptions()
 chrome_settings.add_experimental_option("detach", True)
@@ -28,11 +28,13 @@ time.sleep(1)
 log_in_button2 = driver.find_element(by=By.XPATH, value='//*[@id="organic-div"]/form/div[3]/button')
 log_in_button2.click()
 
-list_of_jobs = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
-print(len(list_of_jobs))
-for job in list_of_jobs:
-    job.click()
+time.sleep(5)
+all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+
+for listing in all_listings:
+    print("Opening Listing")
+    listing.click()
     time.sleep(2)
     save_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-save-button")
     save_button.click()
-
+    time.sleep(2)
